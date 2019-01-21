c1d4142e0e8d5beed69feb07cb6cde1b660e3374
Nathaniel Graham <ngraham20@my.whitworth.edu>
2019-01-20 22:11:28 -0800

    added activities for character and combat pages

---
+++ DnDCombatTracker/app/src/main/java/com/example/dndcombattracker/CombatActivity.java
@@ -0,0 +1,13 @@
+package com.example.dndcombattracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CombatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_combat);
+    }
+}
