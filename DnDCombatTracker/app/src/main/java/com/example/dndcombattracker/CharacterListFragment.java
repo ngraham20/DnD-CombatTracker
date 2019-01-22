a0af77e97fa5dc0f10429afbc2e34268a414420c
Nathaniel Graham <ngraham20@my.whitworth.edu>
2019-01-21 20:41:32 -0800

    fragments and character recycleview work

--- DnDCombatTracker/app/src/main/java/com/example/dndcombattracker/CharacterListFragment.java
+++ DnDCombatTracker/app/src/main/java/com/example/dndcombattracker/CharacterListFragment.java
@@ -4,15 +4,64 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+
 public class CharacterListFragment extends Fragment {
+
+    View view;
+    private RecyclerView mRecyclerView;
+    private ArrayList<Character> mCharacters = new ArrayList<>();
+
+    private static final String TAG = "CharacterListFragment";
+
+    public CharacterListFragment()
+    {
+
+    }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_character_list, null);
+
+        view = inflater.inflate(R.layout.fragment_character_list, container, false);
+        mRecyclerView = (RecyclerView) view.findViewById(R.id.recycler_view);
+        DnDAdapter adapter = new DnDAdapter(getContext(), mCharacters);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        mRecyclerView.setAdapter(adapter);
+        return view;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mCharacters = initDefaultCharacters();
+    }
+
+    private ArrayList<Character> initDefaultCharacters()
+    {
+        ArrayList<Character> characters = new ArrayList<>();
+        characters.add(new PC("Player1", 19, 125, 5));
+        characters.add(new PC("Player2", 19, 125, 5));
+        characters.add(new PC("Player3", 19, 125, 5));
+        characters.add(new PC("Player4", 19, 125, 5));
+        characters.add(new PC("Player5", 19, 125, 5));
+        characters.add(new PC("Player6", 19, 125, 5));
+        characters.add(new PC("Player7", 19, 125, 5));
+        characters.add(new PC("Player8", 19, 125, 5));
+        characters.add(new PC("Player9", 19, 125, 5));
+        characters.add(new PC("Player10", 19, 125, 5));
+        characters.add(new PC("Player11", 19, 125, 5));
+        characters.add(new PC("Player12", 19, 125, 5));
+        characters.add(new PC("Player13", 19, 125, 5));
+
+        return characters;
     }
 }
--- DnDCombatTracker/app/src/main/java/com/example/dndcombattracker/MainActivity.java
+++ DnDCombatTracker/app/src/main/java/com/example/dndcombattracker/MainActivity.java
@@ -5,15 +5,25 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.MenuItem;
-import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+    private static final String TAG = "MainActivity";
+
+    private ArrayList<Character> mCharacters;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d(TAG, "onCreate: started.");
 
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(this);
