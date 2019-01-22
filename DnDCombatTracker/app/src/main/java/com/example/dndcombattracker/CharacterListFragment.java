315c1eb1734b22fff9237c97b4a9a2e4d08a0d4e
Nathaniel Graham <ngraham20@my.whitworth.edu>
2019-01-22 00:33:20 -0800

    FAB is now clickable for character list

--- DnDCombatTracker/app/src/main/java/com/example/dndcombattracker/CharacterListFragment.java
+++ DnDCombatTracker/app/src/main/java/com/example/dndcombattracker/CharacterListFragment.java
@@ -3,13 +3,16 @@ package com.example.dndcombattracker;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -33,8 +36,24 @@ public class CharacterListFragment extends Fragment {
         view = inflater.inflate(R.layout.fragment_character_list, container, false);
         mRecyclerView = (RecyclerView) view.findViewById(R.id.recycler_view);
         DnDAdapter adapter = new DnDAdapter(getContext(), mCharacters);
-        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        mRecyclerView.setLayoutManager(layoutManager);
         mRecyclerView.setAdapter(adapter);
+
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(mRecyclerView.getContext(),
+                layoutManager.getOrientation());
+
+        mRecyclerView.addItemDecoration(dividerItemDecoration);
+
+        FloatingActionButton fab = (FloatingActionButton) view.findViewById(R.id.add_character_button);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(), "FAB Clicked!", Toast.LENGTH_SHORT)
+                        .show();
+            }
+        });
+
         return view;
     }
 
